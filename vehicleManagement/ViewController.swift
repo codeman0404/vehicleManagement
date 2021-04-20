acbc8bce796e30be22c2a9e916192f7e1410508d
Cody Anderson <cody-anderson@uiowa.edu>
2021-04-20 10:01:56 -0500

    Began working on using GPS information on the iPhone.

--- vehicleManagement/ViewController.swift
+++ vehicleManagement/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    //var startDate: Date!
+    var distanceTraveledThisTrip: Double = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello  bob")
         print("hello again bob!")
+        startLocationManager()
         // Do any additional setup after loading the view.
+        
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
+        
+        if lastLocation != nil {
+        
+            if let location = locations.last {
+                
+                distanceTraveledThisTrip += lastLocation.distance(from: location);
+                print("Distance Traveled: " + String(distanceTraveledThisTrip))
+                
+            }
+        }
+        
+        lastLocation = locations.last!
+        print(lastLocation.coordinate)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+       if let error = error as? CLError, error.code == .denied {
+          // Location updates are not authorized.
+          manager.stopUpdatingLocation()
+          print("access was denied... stoping location querying")
+          return
+       }
+    }
+    
+    func startLocationManager(){
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func stopLocationManager(){
+        locationManager.stopUpdatingLocation()
     }
 
 
