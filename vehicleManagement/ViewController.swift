1c48522bc187b8b9387c0f3af8cc3b6e3a8e55df
Cody Anderson <cody-anderson@uiowa.edu>
2021-04-20 09:18:14 -0500

    one more initial commit

--- vehicleManagement/ViewController.swift
+++ vehicleManagement/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello  bob")
+        print("hello again bob!")
         // Do any additional setup after loading the view.
     }
 
