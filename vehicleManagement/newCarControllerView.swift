ff0113552209ed1ec0dfcbcc3fd0acc3222c0eea
lucasduff-code <lucasjduff@gmail.com>
2021-04-28 22:16:06 -0500

    New Vehicle to database now working

--- vehicleManagement/newCarControllerView.swift
+++ vehicleManagement/newCarControllerView.swift
@@ -11,6 +11,8 @@ import FirebaseDatabase
 class newCarControllerView: UIViewController, UITextFieldDelegate {
     private let database = Database.database().reference()
     
+    var userName = "Cody"
+    
     @IBOutlet weak var newVehicleName: UITextField!
     
     @IBOutlet weak var newOdometer: UITextField!
@@ -21,7 +23,51 @@ class newCarControllerView: UIViewController, UITextFieldDelegate {
     
     var newDrivers: [String] = []
     @IBAction func addDriverFuc(_ sender: Any) {
-        newDrivers.append(
+        if let newDriver = self.newAuthorizedDriver.text {
+            newDrivers.append(newDriver)
+        }
+    }
+    @IBAction func submitNewVehicleFunc(_ sender: Any) {
+        if let newVehicle = self.newVehicleName.text{
+            if let newInitOdometer = self.newOdometer.text {
+                self.database.child("cars").child(newVehicle).getData{ (error, snapshot) in
+                    if let error = error {
+                        
+                        print("Error getting data \(error)")
+                        
+                    } else if snapshot.exists() {
+                        print("Vehicle already exists")
+                    }else{
+                        self.database.child("accounts").child(String(self.userName)).child("valid_vehicles").child(newVehicle).setValue(true)
+                        for driver in self.newDrivers{
+                            self.database.child("accounts").child(driver).child("valid_vehicles").child(newVehicle).setValue(true)
+                        }
+                        let object: [String: Any] = [
+                            "coordinates": [
+                                "latitude":0,
+                                "longitude":0
+                                ],
+                            "fuel_level":50,
+                            "isDriving":false,
+                            "miles_since_clear":0,
+                            "odometer":newInitOdometer,
+                            "speed":0
+                            ]
+                        self.database.child("cars").child(newVehicle).setValue(object)
+                    }
+                    
+                }
+                        
+                // self.database.child("accounts").child(String(userName)).
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newVehicleName.delegate = self
+        newOdometer.delegate = self
+        newAuthorizedDriver.delegate = self
     }
     
 }
